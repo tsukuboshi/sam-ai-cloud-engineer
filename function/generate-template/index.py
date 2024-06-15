c2491674652571c8414b3f90c87ead99c522427b
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-06-15 17:59:57 +0900

    テンプレートレビュー処理を追加

--- function/generate-template/index.py
+++ function/generate-template/index.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 import os
 import re
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 import boto3
 import botocore
@@ -30,13 +30,30 @@ def lambda_handler(event: Dict[Any, Any], context: Any) -> Dict[str, Any]:
 
         image_download(input_bucket_name, input_diagram_name, tmp_image_path)
 
-        yaml_content = generate_yaml(tmp_image_path)
+        system_prompt = create_system_prompt()
 
-        template_name = cfn_validate(yaml_content, input_diagram_name, current_time)
+        raw_yaml = generate_yaml(system_prompt, tmp_image_path)
+
+        status = "notvalidated"
+        cfn_res = ""
+        max_review_count = int(os.environ["MAX_REVIEW_COUNT"])
+
+        for review_count in range(max_review_count):
+            logger.info(f"Validation Status: {status}")
+            if status == "normally":
+                break
+            else:
+                modified_yaml = review_yaml(
+                    system_prompt, tmp_image_path, raw_yaml, cfn_res
+                )
+                status, cfn_res = cfn_validate(modified_yaml)
+
+        template_name = f"{input_diagram_name}_{current_time}_{status}.yaml"
+
         tmp_yaml = f"/tmp/{template_name}"
 
         with open(tmp_yaml, "w") as file:
-            file.write(yaml_content)
+            file.write(modified_yaml)
 
         template_upload(template_name, tmp_yaml)
 
@@ -56,13 +73,26 @@ def image_download(bucket_name: str, file_name: str, tmp_file_path: str) -> None
         raise e
 
 
-# YAMLテンプレート内容生成関数
-def generate_yaml(tmp_image_path: str) -> Any:
-    model_id = os.environ["MODEL_ID"]
-    logger.info("Model ID: %s", model_id)
-
+def create_system_prompt() -> str:
     max_token = int(os.environ["MAX_TOKEN"])
     ninety_percent_token = int(max_token / 10 * 9)
+
+    system_prompt = f"""
+    \n回答は以下の条件全てを満たすようにしてください:
+    \n- 必ず回答で出力するCloudFormationテンプレート(yaml形式)の先頭は"```yaml"、末尾は"```"とする。
+    \n- 必要に応じて補足を付与したい場合は、回答で出力するCloudFormationテンプレート内に#を付けてコメントとして記載する。
+    \n- もし回答が{ninety_percent_token}トークンを超えたら、{max_token}トークンに達するまでに一旦回答を分割し、ユーザーが「続き」と入力したら続きの回答を作成する。
+    """
+
+    logger.info("System Prompt: %s", system_prompt)
+    return system_prompt
+
+
+# YAMLテンプレート生成関数
+def generate_yaml(system_prompt: str, tmp_image_path: str) -> Any:
+
+    generate_model_id = os.environ["GENERATE_MODEL_ID"]
+    logger.info("Generate Model ID: %s", generate_model_id)
 
     next_token = None
     type_summaries = []
@@ -80,87 +110,132 @@ def generate_yaml(tmp_image_path: str) -> Any:
         if summary["TypeName"].startswith("AWS::")
     ]
 
-    system_prompt = f"""
-    \n回答は以下の条件全てを満たすようにしてください:
-    \n- 必ず回答で出力するCloudFormationテンプレート(yaml形式)の先頭は"```yaml"、末尾は"```"とする。
-    \n- 必要に応じて補足を付与したい場合は、回答で出力するCloudFormationテンプレート内に#を付けてコメントとして記載する。
-    \n- もし回答が{ninety_percent_token}トークンを超えたら、{max_token}トークンに達するまでに一旦回答を分割し、ユーザーが「続き」と入力したら続きの回答を作成する。
-    \n- 以下の<ResourceTypeList>に存在しないResourceTypeは、回答で出力するCloudFormationテンプレートのResourceType(AWS::service-name::data-type-name)として絶対に使用してはいけない。
-    \n\n<ResourceTypeList>
-    \n{type_list}
-    """
-
-    logger.info("System Prompt: %s", system_prompt)
-
-    first_content_text = f"""
+    first_generate_text = f"""
     \n\nHuman:
-    \n入力されたAWS構成図の詳細情報に基づき、その構成をデプロイするためのCloudFormationテンプレート(YAML形式)を作成してください。
+    \n入力されたAWS構成図に基づき、その構成をデプロイするためのCloudFormationテンプレート(YAML形式)を作成してください。
     \nテンプレートは、以下の条件全てを満たすようにしてください:
     \n- 構成図に記載されている全リソースの設定を必ず含める
     \n- 構成図に基づきリソース間の依存関係や参照を適切に定義する
     \n- 構成図に記載されていないリソースは最低限必要なものを除いて追加しない
+    \n- 以下の<リソースタイプリスト>に存在しないリソースタイプは、回答で出力するCloudFormationテンプレートのリソースタイプとして使用してはいけない。
+    \n\n<リソースタイプリスト>
+    \n{type_list}
     """
-
-    logger.info("First Message: %s", first_content_text)
 
     messages: List[Dict[str, Any]] = []
 
     try:
-        first_res_message = request_bedrock(
-            model_id,
+        row_yaml = output_yaml(
+            generate_model_id,
             messages,
-            first_content_text,
+            first_generate_text,
             system_prompt,
             tmp_image_path,
         )
 
-        first_row_content = first_res_message["content"][0]["text"]
-        first_yaml_content = format_yaml(first_row_content)
-
-        logger.info(f"First YAML: %s", first_yaml_content)
-
-        first_yaml_length = len(first_yaml_content)
-        eighty_percent_first_yaml_length = int(first_yaml_length * 0.8)
-
-        messages.append(first_res_message)
-
-        yaml_content = first_yaml_content
-
-        max_count = int(os.environ["MAX_COUNT"])
-
-        next_content_text = "続き"
-
-        for count in range(max_count):
-            next_res_message = request_bedrock(
-                model_id, messages, next_content_text, system_prompt
-            )
-
-            next_row_content = next_res_message["content"][0]["text"]
-            next_yaml_content = format_yaml(next_row_content)
-
-            logger.info(f"Next YAML {count}: %s", next_yaml_content)
-            yaml_content += "\n" + next_yaml_content
-
-            next_yaml_length = len(next_yaml_content)
-
-            if next_yaml_length > eighty_percent_first_yaml_length:
-                logger.info(
-                    f"Next YAML Length {next_yaml_length} is over 80% of First YAML Length {eighty_percent_first_yaml_length}, continuing..."
-                )
-                messages.append(next_res_message)
-            else:
-                logger.info(
-                    f"Next YAML Length {next_yaml_length} is under 80% of First YAML Length {eighty_percent_first_yaml_length}, breaking..."
-                )
-                break
-
-        logger.info("Combined YAML: %s", yaml_content)
-
-        return yaml_content
+        return row_yaml
 
     except botocore.exceptions.ClientError as e:
         logger.error("Bedrock Request Error: %s", e)
         raise e
+
+
+# YAMLテンプレートレビュー関数
+def review_yaml(
+    system_prompt: str, tmp_image_path: str, yaml_content: str, cfn_err: Any
+) -> Any:
+    review_model_id = os.environ["REVIEW_MODEL_ID"]
+    logger.info("Review Model ID: %s", review_model_id)
+
+    first_review_text = f"""
+    \n\nHuman:
+    \n入力されたAWS構成図、提示されたCloudFormationテンプレートを確認し、構成図に基づいてテンプレートが適切に作成されているか確認してください。
+    \nもしテンプレートに問題がない場合は、提示されたテンプレートをそのまま全て出力してください。
+    \nもしテンプレートに問題がある場合は、問題がある箇所のみ修正した上で修正後のテンプレートを全て出力してください。
+    \nもしエラーメッセージが何かしら追加で提示されている場合は、エラーを解消できるように修正したテンプレートを出力してください。
+    \n\n<CloudFormationテンプレート>
+    \n{yaml_content}
+    \n\n<エラーメッセージ>
+    \n{cfn_err}
+    """
+
+    messages: List[Dict[str, Any]] = []
+
+    try:
+        modified_yaml = output_yaml(
+            review_model_id,
+            messages,
+            first_review_text,
+            system_prompt,
+            tmp_image_path,
+        )
+        return modified_yaml
+
+    except botocore.exceptions.ClientError as e:
+        logger.error("Bedrock Request Error: %s", e)
+        raise e
+
+
+def output_yaml(
+    model_id: str,
+    messages: List[Dict[str, Any]],
+    first_content_text: str,
+    system_prompt: str | None = None,
+    tmp_image_path: str | None = None,
+) -> Any:
+
+    logger.info("First Request: %s", first_content_text)
+
+    first_res_message = request_bedrock(
+        model_id,
+        messages,
+        first_content_text,
+        system_prompt,
+        tmp_image_path,
+    )
+
+    first_row_content = first_res_message["content"][0]["text"]
+    logger.info("First Response: %s", first_row_content)
+
+    first_yaml_content = format_yaml(first_row_content)
+
+    first_yaml_length = len(first_yaml_content)
+    eighty_percent_first_yaml_length = int(first_yaml_length * 0.8)
+
+    messages.append(first_res_message)
+
+    yaml_content = first_yaml_content
+
+    next_content_text = "続き"
+    max_yaml_count = int(os.environ["MAX_YAML_COUNT"])
+
+    for yaml_count in range(max_yaml_count):
+        next_res_message = request_bedrock(
+            model_id, messages, next_content_text, system_prompt
+        )
+
+        next_row_content = next_res_message["content"][0]["text"]
+        logger.info(f"Next Response {yaml_count}: %s", next_row_content)
+        next_yaml_content = format_yaml(next_row_content)
+
+        yaml_content += "\n" + next_yaml_content
+
+        next_yaml_length = len(next_yaml_content)
+
+        if next_yaml_length > eighty_percent_first_yaml_length:
+            logger.info(
+                f"Next YAML Length {next_yaml_length} is over 80% of First YAML Length {eighty_percent_first_yaml_length}, continuing..."
+            )
+            messages.append(next_res_message)
+        else:
+            logger.info(
+                f"Next YAML Length {next_yaml_length} is under 80% of First YAML Length {eighty_percent_first_yaml_length}, breaking..."
+            )
+            break
+
+    logger.info("Combined YAML: %s", yaml_content)
+
+    return yaml_content
 
 
 def request_bedrock(
@@ -228,25 +303,24 @@ def format_yaml(row_content: str) -> str:
     match = re.search(r"```yaml\n(.*?)\n```", row_content, re.DOTALL)
     if match:
         response_text = match.group(1)
-        logger.info("Template: %s", response_text)
         return response_text
     else:
         return ""
 
 
 # CloudFormationバリデーション関数
-def cfn_validate(yaml_content: str, input_diagram_name: str, current_time: str) -> str:
+def cfn_validate(yaml_content: str) -> Tuple[Any, str]:
     try:
         cfn_res = cfn.validate_template(
             TemplateBody=yaml_content,
         )
-        logger.info("CloudFormation: %s", cfn_res)
-        file_name = f"{input_diagram_name}_{current_time}_normally.yaml"
-        return file_name
-    except botocore.exceptions.ClientError as e:
-        logger.warning("CloudFormation Validation Error: %s", e)
-        file_name = f"{input_diagram_name}_{current_time}_error.yaml"
-        return file_name
+        status = "normally"
+        logger.info(f"CloudFormation is {status}: %s", cfn_res)
+        return status, cfn_res
+    except botocore.exceptions.ClientError as cfn_err:
+        status = "error"
+        logger.warning(f"CloudFormation is {status}: %s", cfn_err)
+        return status, cfn_err
 
 
 # テンプレートアップロード関数
